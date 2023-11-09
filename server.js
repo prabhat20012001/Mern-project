2835334a9c6554eff225e746b8b21a6f3accb944
Prabhat kumar <prabhatkumarmnnit@gmail.com>
2023-11-10 01:41:05 +0530

    user schema is created..

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ app.get("/",(req,res)=>{
 }
 )
 
+const userSchema=new mongoose.Schema({
+name:{
+    type:String,
+    require:true
+},
+email:{
+    type:String,
+    unique:true,
+    require:true
+},
+password:{
+    type:String,
+    require:true
+},
+createdAt:{
+    type:Date,
+    default:Date.now
+}
+
+})
+
+const user=mongoose.model("user",userSchema)
+
 
 const port=4000
 
