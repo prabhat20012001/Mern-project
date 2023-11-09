fccab663bc5d3087122a9c9006a1539bcc3e140c
Prabhat kumar <prabhatkumarmnnit@gmail.com>
2023-11-10 01:31:58 +0530

    database connected

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
+import mongoose from "mongoose"
 
 const app=express()
+mongoose.connect("mongodb+srv://prabhatkumarmnnit:MOk1moM1vGV9xlXq@mernproject.ryclxt8.mongodb.net/",{dbName:"MernProject"}).then(()=>console.log("mongodb is connected .."))
 
 app.get("/",(req,res)=>{
     res.send("Hello world")
