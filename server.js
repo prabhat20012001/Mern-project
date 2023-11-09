de0d7e063ea9ddd81386b5a0f4804c6413064062
Prabhat kumar <prabhatkumarmnnit@gmail.com>
2023-11-10 02:02:19 +0530

    user created and get route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express"
 import mongoose from "mongoose"
-
+import jsonParser from "json-parser"
 const app=express()
+
+app.use(express.json());
+
 mongoose.connect("mongodb+srv://prabhatkumarmnnit:MOk1moM1vGV9xlXq@mernproject.ryclxt8.mongodb.net/",{dbName:"MernProject"}).then(()=>console.log("mongodb is connected .."))
 
 app.get("/",(req,res)=>{
@@ -30,8 +33,25 @@ createdAt:{
 
 })
 
-const user=mongoose.model("user",userSchema)
+const User=mongoose.model("user",userSchema)
 
+app.post("/api/users/register",async(req,res)=>{
+    const {name,email,password}=req.body
+
+    const user=await User.create({
+        name,email,password
+    })
+    res.json({
+        success:"user created"
+    })
+})
+
+
+app.get("/api/users/getUser",async(req,res)=>{
+    const user=await User.find()
+    res.send(user)
+
+})
 
 const port=4000
 
