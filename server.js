f3d9d6af95d5a9a9b7ded9059c52346ea264a6ed
Prabhat kumar <prabhatkumarmnnit@gmail.com>
2023-11-10 13:32:50 +0530

    set up for deployment

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose"
 import userRouter from "./routes/user.js"
 import cookieParser from "cookie-parser"
 import blogRoutes from "./routes/blog.js"
+import { config } from "dotenv"
 const app=express()
 
 app.use(express.json());
 app.use(cookieParser())
-mongoose.connect("mongodb+srv://prabhatkumarmnnit:MOk1moM1vGV9xlXq@mernproject.ryclxt8.mongodb.net/",{dbName:"MernProject"}).then(()=>console.log("mongodb is connected .."))
+config({
+    path:"./data/config.env"
+})
+mongoose.connect(process.env.MONGO_URL,{dbName:"MernProject"}).then(()=>console.log("mongodb is connected .."))
 
 // user router
 app.use("/api/users",userRouter)
@@ -16,4 +20,4 @@ app.use("/api/users",userRouter)
 app.use("/api/blogs",blogRoutes)
 const port=4000
 
-app.listen(port,()=>console.log(`server is running on ${port}`))
+app.listen(process.env.PORT,()=>console.log(`server is running on ${process.env.PORT}`))
