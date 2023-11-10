0522f0fb8d8f666ecf3dfe37d15599a85b23c77f
Prabhat kumar <prabhatkumarmnnit@gmail.com>
2023-11-11 02:42:43 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import blogRoutes from "./routes/blog.js"
 import { config } from "dotenv"
 import cors from "cors"
 const app=express()
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
